a0d72712ff6e66fa742e4393ff1b2094227b53d0
vedpatil611 <vedpatil611@gmail.com>
2021-07-23 15:40:27 +0530

    Dataframe reader

--- src/series_reader/strings/mod.rs
+++ src/series_reader/strings/mod.rs
@@ -1,8 +1,19 @@
+use serde::{Serialize, Deserialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-struct SeriesStrReader
+#[derive(Serialize, Deserialize)]
+pub struct SeriesStrReader
 {
     name: String,
     data: Vec<Option<String>>,
+}
+
+impl SeriesStrReader {
+    pub fn new(name: String, data: Vec<Option<String>>) -> SeriesStrReader {
+        SeriesStrReader {
+            name,
+            data,
+        }
+    }
 }
