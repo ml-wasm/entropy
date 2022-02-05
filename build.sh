939d8d4e293fc24c572a9d2535b81cbd4d746ad4
dpirad007 <dionpinto000@gmail.com>
2022-02-05 20:00:45 +0530

    Script fix

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
 	rustup run nightly \
 	wasm-pack build --target web --scope ml.wasm \
-	-- -Z build-std=panic_abort,std
+	-- . -Z build-std=panic_abort,std
